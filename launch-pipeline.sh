4ed36d3a31b9a4cac0d7f4638f9fb5d448103db8
Amine RACHYD <aminerachyd99@gmail.com>
2022-04-28 17:53:22 +0200

    Updated task images + launch script
    Updated the general pipeline to run remote pipelines on 3 clusters (x86,
    Power and Z).
    Launch script uses OpenShift server URLs another script (servers.sh).
    Updated build images for remote task to use multi-architecture images.

--- launch-pipeline.sh
+++ launch-pipeline.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+# Fetch server urls
+source servers.sh
 
 # Create variables
 echo "Enter the git repository url to run the pipeline on"
 read GIT_URL
 APP_NAME=$(basename "$GIT_URL" | sed s/".git"//)
 PIPELINE_NAME=$APP_NAME-multiarch-build
+_SERVER_URL=api.apps.diamond.edu.ihost.com:6443
 echo "Enter the image registry where you want to push your image"
 read IMAGE_SERVER
 echo "Enter the name of your user/organisation on your registry"
 read IMAGE_NAMESPACE
-echo "Enter the api server url for your x86 cluster"
-read X86_SERVER_URL
+#echo "Enter the api server url for your x86 cluster"
+#read X86_SERVER_URL
+#echo "Enter the api server url for your Power cluster"
+#read POWER_SERVER_URL
+#echo "Enter the api server url for your Z cluster"
+#read Z_SERVER_URL
 echo "Enter the protocol where you want to check the health status of your application, should be either https or grpc"
 read HEALTH_PROTOCOL
 
@@ -24,6 +31,8 @@ tkn pipeline start $PIPELINE_NAME \
   --param image-server=$IMAGE_SERVER \
   --param image-namespace=$IMAGE_NAMESPACE \
   --param x86-server-url=$X86_SERVER_URL \
+  --param power-server-url=$POWER_SERVER_URL \
+  --param z-server-url=$Z_SERVER_URL \
   --param health-protocol=$HEALTH_PROTOCOL \
 
 # TODO Create webhook to trigger the pipeline on repo push event
