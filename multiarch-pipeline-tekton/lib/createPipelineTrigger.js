bcc629213207667048a7fdc8280e4c93f2e98ca2
Amine RACHYD <aminerachyd99@gmail.com>
2022-05-11 12:00:31 +0200

    mpt creates pipeline triggers for pipelines
    Need to create github webhooks and point them into smee.io

--- multiarch-pipeline-tekton/lib/createPipelineTrigger.js
+++ multiarch-pipeline-tekton/lib/createPipelineTrigger.js
@@ -16,17 +16,26 @@ spec:
       value: $(body.repository.url)
 `;
 
-// TODO
+// TODO Create a github webhook for the pipeline
 // Update the eventlistener to be able to handle multiple events from different repositories
 module.exports = createPipelineTrigger = ({
   pipelineName,
+  apiServerPower,
+  apiServerX86,
+  apiServerZ,
+  gitUrl,
+  imageRegistry,
+  healthProtocol,
   namespace,
+  buildOnX86,
+  buildOnPower,
+  buildOnZ,
   appName,
 }) => {
   // Create the triggerbinding file and write it to the directory
   const triggerBindingPath = `${process.env.HOME}/.mpt/applied-pipelines/triggers/triggerbinding.yaml`;
-  const eventListenerPath = `${process.env.HOME}/.mpt/applied-pipelines/triggers/eventListener.yaml`;
   const triggerTemplatePath = `${process.env.HOME}/.mpt/applied-pipelines/triggers/triggerTemplate-${appName}.yaml`;
+  const eventListenerPath = `${process.env.HOME}/.mpt/applied-pipelines/triggers/eventListener.yaml`;
 
   // Create the tekton event listener
   const eventListener = `
@@ -44,18 +53,18 @@ spec:
         ref: trigger-binding
       interceptors:
         - params:
-          - name: filter
-            value: >-
-              header.match('X-GitHub-Event', 'push') && body.ref == 'refs/heads/master' && body.repository.full_name == '${namespace}/${appName}'
-          - name: overlays
-            value: null
+            - name: filter
+              value: >-
+                header.match('X-GitHub-Event', 'push') && body.repository.full_name ==
+                '${namespace}/${appName}'
+            - name: overlays
+              value: null
           ref: 
             kind: ClusterInterceptor
             name: cel
       name: ${namespace}-${appName}-master
       template:
         ref: ${appName}
-        
 `;
   // Create the trigger template
   const triggerTemplate = `
@@ -82,15 +91,83 @@ spec:
             value: $(tt.params.gitrepositoryurl)
           - name: git-revision
             value: $(tt.params.gitrevision)
+          - name: image-server
+            value: ${imageRegistry}
+          - name: image-namespace
+            value: ${namespace}
+          ${
+            buildOnX86
+              ? `
+          - name: x86-server-url
+            value: ${apiServerX86}
+          `
+              : ""
+          }
+          ${
+            buildOnPower
+              ? `
+          - name: power-server-url
+            value: ${apiServerPower}
+          `
+              : ""
+          }
+          ${
+            buildOnZ
+              ? `
+          - name: z-server-url
+            value: ${apiServerZ}
+          `
+              : ""
+          }
+          - name: health-protocol
+            value: ${healthProtocol}
         pipelineRef:
           name: ${pipelineName}
 `;
 
+  const pEventListener = {
+    spec: {
+      triggers: [
+        {
+          bindings: [
+            {
+              kind: "TriggerBinding",
+              ref: "trigger-binding",
+            },
+          ],
+          interceptors: [
+            {
+              params: [
+                {
+                  name: "filter",
+                  value: `header.match('X-GitHub-Event', 'push') && body.repository.full_name == '${namespace}/${appName}'`,
+                },
+                {
+                  name: "overlays",
+                  value: null,
+                },
+              ],
+              ref: {
+                kind: "ClusterInterceptor",
+                name: "cel",
+              },
+            },
+          ],
+          name: `${namespace}-${appName}-master`,
+          template: {
+            ref: `${appName}`,
+          },
+        },
+      ],
+    },
+  };
+
   // If it exists, patch it with the new trigger
   try {
     fs.writeFileSync(triggerBindingPath, triggerBinding);
-    fs.writeFileSync(eventListenerPath, eventListener);
     fs.writeFileSync(triggerTemplatePath, triggerTemplate);
+    fs.writeFileSync(eventListenerPath, eventListener);
+    // fs.writeFileSync(patchEventListenerPath, pEventListener);
   } catch (error) {
     const errmsg = `Failed to write file: ${error}`;
     throw new Error(errmsg);
@@ -98,8 +175,8 @@ spec:
 
   // Apply the files
   const applyTriggerBinding = `oc apply -f ${triggerBindingPath}`;
-  const applyEventListener = `oc apply -f ${eventListenerPath}`;
   const applyTriggerTemplate = `oc apply -f ${triggerTemplatePath}`;
+  const applyEventListener = `oc apply -f ${eventListenerPath}`;
 
   exec(applyTriggerBinding, (err, stdout, stderr) => {
     if (err) {
@@ -108,11 +185,44 @@ spec:
     console.log(stdout);
   });
 
-  exec(applyEventListener, (err, stdout, stderr) => {
+  // Check if the event listener exists on the cluster
+  exec(`oc get eventlistener event-listener -o json`, (err, stdout, stderr) => {
     if (err) {
-      throw err;
+      console.log("Event listener does not exist, creating one");
+      exec(applyEventListener, (err, stdout, stderr) => {
+        if (err) {
+          throw err;
+        }
+        console.log(stdout);
+      });
+    } else {
+      // Patch the event listener
+      console.log("Event listener exists, patching it");
+      const existingEventListener = JSON.parse(stdout);
+      const newTriggers = [
+        ...[...existingEventListener.spec.triggers].filter(
+          (trigger) => trigger.name !== `${namespace}-${appName}-master`
+        ),
+        ...pEventListener.spec.triggers,
+      ];
+
+      const newEventListener = {
+        ...existingEventListener,
+        spec: {
+          ...existingEventListener.spec,
+          triggers: newTriggers,
+        },
+      };
+      fs.writeFileSync(eventListenerPath, JSON.stringify(newEventListener));
+      const patchEventListener = `oc apply -f ${eventListenerPath}`;
+      // Create patch file with the new trigger
+      exec(patchEventListener, (err, stdout, stderr) => {
+        if (err) {
+          throw err;
+        }
+        console.log(stdout);
+      });
     }
-    console.log(stdout);
   });
 
   exec(applyTriggerTemplate, (err, stdout, stderr) => {
